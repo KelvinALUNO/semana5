e849fcbdf226c83eac756c1ec85665c55e5652cf
kelvin vieira lima <kelvin_v_lima@estudante.sesisenai.org.br>
2024-07-09 10:32:39 -0300

    a

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const array = ["futebol", "jogos"]
+const myArray = JSON.stringify(array)
+localStorage.setItem("meus-interesses", myArray)
+const dados = localStorage.getItem("meus-interesses")
+const dadosSalvos = JSON.parse(dados)
+const ul = document.querySelector('ul');
+ul.innerHTML = '';
 function adicionar() {
     const ul = document.querySelector("ul")
     const li = document.createElement("li")
@@ -6,10 +13,51 @@ function adicionar() {
     li.innerText = tarefa
     ul.appendChild(li)
     input.value = ""
-   const delItem = document.createElement('button')
+    const delItem = document.createElement('button')
     delItem.textContent = 'remover'
     delItem.onclick = function() {
-    ul.removeChild(li)
+     ul.removeChild(li)
     }
     li.appendChild(delItem)
+    
+    const valor = tarefa
+    let arrayValores = obterArrayDoLocal()
+    arrayValores.push(valor)
+    salvarArrayNoLocal(arrayValores);
+    const btnSave = document.querySelector("#add")
+    btnSave.addEventListener("click", () => {
+        let dados = [];
+        if (localStorage.hasOwnProperty("meus-interesses")){
+            dadosSalvos = JSON.parse(localStorage.getItem("meus-interesses"))
+            dadosSalvos.push({ name: input.value })
+            localStorage.setItem("meus-interesses", JSON.stringify(myArray))
+        }
+    })
 }
+async function recuperarDados() {
+    try {
+        dadosSalvos.forEach((dadosSalvos) => {
+            const li = document.createElement('li')
+            li.textContent = dadosSalvos
+            ul.appendChild(li)
+            const delItem = document.createElement('button')
+            delItem.textContent = 'remover'
+            delItem.onclick = function() {
+                ul.removeChild(li)
+            }  
+            li.appendChild(delItem)
+        })  
+    }
+    catch {
+        alert("erro")
+    }
+} 
+recuperarDados()
+function obterArrayDoLocal() {
+    const arrayString = localStorage.getItem("meus-interesses");
+    return arrayString ? JSON.parse(arrayString) : [];
+}
+function salvarArrayNoLocal(array) {
+    localStorage.setItem("meus-interesses", JSON.stringify(array));
+}
+  
